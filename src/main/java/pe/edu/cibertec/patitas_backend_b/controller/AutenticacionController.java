154f07d035750a1c4f03a21284c402863e85f5e5
Carlos Yaringaño <carlos138531@gmail.com>
2024-10-05 15:37:33 -0500

    Actualización del Controller con tiempos de ejecución

--- src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
+++ src/main/java/pe/edu/cibertec/patitas_backend_b/controller/AutenticacionController.java
@@ -1,21 +1,40 @@
 package pe.edu.cibertec.patitas_backend_b.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_backend_b.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_backend_b.service.AutenticacionService;
+
+import java.time.Duration;
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/autenticacion")
 public class AutenticacionController {
+    @Autowired
+    AutenticacionService autenticacionService;
 
     @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO){
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
+        try {
 
-        return new LoginResponseDTO("00", "", "Julio Chavez", "julio.chavez@gmail.com");
+            Thread.sleep(Duration.ofSeconds(5));
+            String[] datosUsuario = autenticacionService.validarUsuario(loginRequestDTO);
+            System.out.println("Respuesta: "+ Arrays.toString(datosUsuario));
+            if (datosUsuario == null) {
 
+                return new LoginResponseDTO("01", "Error: usuario no encontrado", "", "");
+            }
+            return new LoginResponseDTO("00", "", datosUsuario[0], datosUsuario[1]);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return new LoginResponseDTO("00", "", "Julio Chavez", "julio.chavez@gmail.com");
+
+        }
     }
 
 }
